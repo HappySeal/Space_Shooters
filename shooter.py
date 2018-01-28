ebb90228dd99d3e7ba895fbd728c2b8a2d4d9f50
HappySeal <cafer15165@gmail.com>
2018-01-29 00:08:54 +0300

    V 1.1.0
    Enemies !

--- shooter.py
+++ shooter.py
@@ -1,4 +1,4 @@
-import pygame,sys,os,math
+import pygame,sys,os,math,time
 pygame.init()
 displayW=650
 displayH=650
@@ -13,10 +13,64 @@ WHITE=(255,255,255)
 jetImg = pygame.image.load("Jet.gif")
 laserImg = pygame.image.load("Laser.png")
 laserWav = pygame.mixer.Sound("laser.wav")
+badjetImg = pygame.image.load("JetBad.png")
+mainJetX = displayH/2 - jetImg.get_rect().center[0]
+mainJetY = displayH/2 - jetImg.get_rect().center[1]
+lasers = []
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.hp = 10
+        self.org_enemy = badjetImg
+        self.image = self.org_enemy.copy()
+        self.rect = self.image.get_rect(center=(x,y))
+        self.move = [self.rect.x,self.rect.y]
+        self.angleDeg = self.get_angle()
+        self.angleRad = math.radians(self.angleDeg)
+        self.speed_multiplier = 4
+        self.speed = ((self.speed_multiplier*math.sin(self.angleRad)),
+                      (self.speed_multiplier*math.cos(self.angleRad)))
+    def absCenter(self):
+        return self.org_enemy.get_rect().center[0]+int(self.move[0]),self.org_enemy.get_rect().center[1]+int(self.move[1])
+    def get_angle(self):
+        pos1 = (mainJetY-self.absCenter()[1],mainJetX-self.absCenter()[0])
+
+        deg = 270-math.degrees(math.atan2(*pos1))
+        self.image = pygame.transform.rotate(self.org_enemy,deg)
+        return 270-math.degrees(math.atan2(*pos1))
+    def remove(self,display):
+        for laser in lasers:
+             if pygame.sprite.collide_rect(self,laser):
+                lasers.remove(laser)
+                laser.kill()
+                self.hp -= 2
+                if self.hp <= 0:
+                    self.kill()
+
+    def moveCal(self):
+        self.absCenter()
+        self.angleDeg = self.get_angle()
+        self.angleRad = math.radians(self.angleDeg)
+        self.speed = (-(self.speed_multiplier*math.sin(self.angleRad)),
+                      -(self.speed_multiplier*math.cos(self.angleRad)))
+        self.move[0] += self.speed[0]
+        self.move[1] += self.speed[1]
+        self.rect.topleft = self.move
+    def update(self,display):
+        self.remove(display)
+        self.moveCal()
+
+    def draw(self):
+        gameDisplay.blit(self.image,(self.x,self.y))
+    def drawLine(self):
+        pygame.draw.line(gameDisplay,GREEN,(self.absCenter()[0],self.absCenter()[1]),(mainJetX,mainJetY))
 class Laser(pygame.sprite.Sprite):
     def __init__(self,x,y,angle):
         pygame.sprite.Sprite.__init__(self)
+        lasers.append(self)
         laserWav.set_volume(0.5)
         laserWav.play()
         self.org_laser = laserImg
@@ -35,8 +89,8 @@ class Laser(pygame.sprite.Sprite):
 
     def remove(self,display):
         if not self.rect.colliderect(display):
+            lasers.remove(self)
             self.kill()
-
 class Jet(object):
     def __init__(self,x,y,org_image):
         self.x = x
@@ -45,6 +99,8 @@ class Jet(object):
         self.image = self.org_image.copy()
         self.rect = self.image.get_rect(center=(self.x,self.y))
         self.angle = self.get_angle(pygame.mouse.get_pos())
+        self.cooldown = 0.000000001
+        self.lastTime = time.time()
     def absCenter(self):
         return self.image.get_rect().center[0]+self.x,self.image.get_rect().center[1]+self.y
     def draw(self):
@@ -61,8 +117,15 @@ class Jet(object):
         self.x+= speedx
         self.y += speedy
     def get_event(self,event,objects):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            objects.add(Laser(self.absCenter()[0],self.absCenter()[1],self.angle))
+        if pygame.mouse.get_pressed()[0]:
+            try:
+                while time.time() - self.lastTime >= self.cooldown:
+                    objects.add(Laser(self.absCenter()[0],self.absCenter()[1],self.angle))
+                    self.lastTime = time.time()
+            except AttributeError:
+                pass
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            objects.add(Enemy(64,64))
 class Game(object):
     def __init__(self,displayW,displayH):
         self.screen = pygame.display.get_surface()
@@ -84,8 +147,9 @@ class Game(object):
     def update(self):
         self.objects.update(self.screen_rect)
     def Main(self):
+        global mainJetY,mainJetX
         error = False
-        jet = Jet(displayH/2 - jetImg.get_rect().center[0],displayW/2 - jetImg.get_rect().center[0],jetImg)
+        jet = Jet(mainJetX,mainJetY,jetImg)
         jet.draw()
         speedx = 0
         speedy =0
@@ -116,7 +180,10 @@ class Game(object):
                 jet.get_event(event,self.objects)
             jet.move(speedx,speedy)
             jet.draw()
+            mainJetX = jet.x + jetImg.get_rect().center[0]
+            mainJetY = jet.y + jetImg.get_rect().center[1]
             #jet.drawLine(pygame.mouse.get_pos())
+
             self.update()
             self.objects.draw(self.screen)
             caption = "{} - FPS :{:.2f}".format(CAPTION,clock.get_fps())
